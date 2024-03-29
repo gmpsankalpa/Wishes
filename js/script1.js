3d8cf0308c5030bafb17daf4e9f1ac15ec852dfd
GMP Sankalpa <malithapiumal02@gmail.com>
2024-03-29 23:46:39 +0530

    loding add

--- js/script1.js
+++ js/script1.js
@@ -1,9 +1,19 @@
-// Function to handle button click event and redirection
-document.getElementById('redirectButton').addEventListener('click', redirectToPage);
+// Function to handle button click event, redirection, and loading overlay
+document.getElementById('redirectButton').addEventListener('click', function() {
+    // Show loading overlay
+    document.getElementById('loadingOverlay').style.display = 'flex';
 
-function redirectToPage() {
     // Add the 'openAnimation' class to trigger the animation
     document.getElementById('envelope').classList.add('openAnimation');
+
     // Replace 'https://example.com' with the URL you want to redirect to
-    setTimeout(() => { window.location.href = './wish.html'; }, 500); // Delaying the redirection for the duration of the animation
-}
+    setTimeout(() => { 
+        // Simulate some loading time, you would replace this with your actual loading process
+        setTimeout(() => {
+            // Hide loading overlay
+            document.getElementById('loadingOverlay').style.display = 'none';
+            // Redirect to the desired page
+            window.location.href = './wish.html';
+        }, 2500); // Adjust the time as needed for loading animation
+    }, 2000); // Delaying the redirection for the duration of the animation
+});
