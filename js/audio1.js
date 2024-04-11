c9abe0773cadf00abbaeb6aed0a320c681f9e28e
GMP Sankalpa <malithapiumal02@gmail.com>
2024-04-11 08:26:23 +0530

    fix

--- js/audio1.js
+++ js/audio1.js
@@ -1,10 +1,21 @@
 let myAudio = new Audio('https://song.sgp1.digitaloceanspaces.com/song/645/6792.mp3'); // Create an Audio object with the file 'Ceylon.mp3'
 
 window.onload = function() {
-    // Display alert message when the page is loaded
-    alert("🎶Before you proceed, we recommend turning up your device's volume to fully immerse yourself in the enhanced audio experiences we have to offer!\n🎶ඔබ ඉදිරියට යාමට පෙර, අප විසින් පිරිනමනු ලබන වැඩිදියුණු කළ ශ්‍රව්‍ය අත්දැකීම්වල සම්පූර්ණයෙන්ම ගිල්වීමට ඔබගේ උපාංගයේ ශබ්දය වැඩි කිරීමට අපි නිර්දේශ කරමු!");
+    // Create a button element
+    let playButton = document.createElement("button");
+    playButton.textContent = "Click to play audio";
 
-    // When user clicks "OK", start the game loop
+    // Append the button to the document body
+    document.body.appendChild(playButton);
+
+    // When the button is clicked, start playing the audio
+    playButton.onclick = function() {
+        myAudio.play(); // Start playing the audio
+        // Once audio starts playing, remove the button
+        playButton.style.display = "none";
+    };
+
+    // Start the game loop
     setInterval(GameLoop, 1000 / 10);
 };
 
