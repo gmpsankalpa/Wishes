0720a51bc3b21b1ee1268deb8a1672fd9d8ea0c7
GMP Sankalpa <malithapiumal02@gmail.com>
2024-04-11 08:42:17 +0530

    fix

--- js/audio1.js
+++ js/audio1.js
@@ -4,6 +4,12 @@ window.onload = function() {
     // Display alert message when the page is loaded
     alert("🎶Before you proceed, we recommend turning up your device's volume to fully immerse yourself in the enhanced audio experiences we have to offer!\n🎶ඔබ ඉදිරියට යාමට පෙර, අප විසින් පිරිනමනු ලබන වැඩිදියුණු කළ ශ්‍රව්‍ය අත්දැකීම්වල සම්පූර්ණයෙන්ම ගිල්වීමට ඔබගේ උපාංගයේ ශබ්දය වැඩි කිරීමට අපි නිර්දේශ කරමු!");
 
-    // When user clicks "OK", start playing the audio
-    myAudio.play();
+    // When user clicks "OK", start the game loop
+    setInterval(GameLoop, 1000 / 10);
 };
+
+function GameLoop() {
+    if (myAudio.paused) { // Check if the audio is paused
+        myAudio.play(); // If paused, play the audio
+    }
+}
