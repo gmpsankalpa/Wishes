50f6daedb1c29cf9637f6ae5494f45bbaa81468f
GMP Sankalpa <malithapiumal02@gmail.com>
2024-04-12 16:04:32 +0530

    fix

--- js/loading.js
+++ js/loading.js
@@ -1,8 +1,8 @@
 // Hide the loader when the page is fully loaded
-// window.addEventListener('load', function() {
-//   var loader = document.getElementById('loader');
-//   loader.style.display = 'none';
-// });
+window.addEventListener('load', function() {
+  var loader = document.getElementById('loader');
+  loader.style.display = 'none';
+});
 
 window.addEventListener('load', function() {
   var loader = document.getElementById('loader1');
