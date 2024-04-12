23c6b68d4b304e69f64879ba6069f1a74dd66bc3
GMP Sankalpa <malithapiumal02@gmail.com>
2024-04-12 20:26:07 +0530

    bug fix

--- js/loading1.js
+++ js/loading1.js
@@ -1,22 +1,5 @@
-// Get reference to loading overlay element
-var loadingOverlay = document.getElementById("loadingOverlay");
-
-// Function to show loading overlay
-function showLoadingOverlay() {
-  loadingOverlay.style.display = "flex"; // or "block" depending on your layout needs
-}
-
-// Function to hide loading overlay
-function hideLoadingOverlay() {
-  loadingOverlay.style.display = "none";
-}
-
-// Show loading overlay when the DOM content is loading
-document.addEventListener("DOMContentLoaded", function() {
-  showLoadingOverlay();
-});
-
-// Hide loading overlay when the webpage has fully loaded
-window.addEventListener("load", function() {
-  hideLoadingOverlay();
+// Hide the loader when the page is fully loaded
+window.addEventListener('load', function() {
+  var loader = document.getElementById('loader');
+  loader.style.display = 'none';
 });
