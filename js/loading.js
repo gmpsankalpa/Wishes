18d958599fd9c9880bf718c052c1bb50b49aed21
GMP Sankalpa <malithapiumal02@gmail.com>
2024-04-12 14:22:17 +0530

    loading fix

--- js/loading.js
+++ js/loading.js
@@ -3,3 +3,8 @@ window.addEventListener('load', function() {
   var loader = document.getElementById('loader');
   loader.style.display = 'none';
 });
+
+window.addEventListener('load', function() {
+  var loader = document.getElementById('loader1');
+  loader.style.display = 'none';
+});
